626bcd37987ec799bbc35b643a8e52774b63d06d
update-branch <josephodada@gmail.com>
2024-08-23 09:20:59 +0100

    Basic app directory and files

---
+++ solar_power_lstm_model.py
@@ -0,0 +1,269 @@
+import pandas as pd
+
+# Import necessary libraries
+import pandas as pd
+import numpy as np
+import seaborn as sns 
+import matplotlib.pyplot as plt
+#%matplotlib inline 
+
+import warnings
+warnings.filterwarnings("ignore")
+generation_data = pd.read_csv('data/Plant_2_Generation_Data.csv')
+weather_data = pd.read_csv('data/Plant_2_Weather_Sensor_Data.csv')
+
+print(generation_data.head())
+generation_data['DATE_TIME'] = pd.to_datetime(generation_data['DATE_TIME'],format = 'mixed') # '%Y-%m-%d %H:%M')
+weather_data['DATE_TIME'] = pd.to_datetime(weather_data['DATE_TIME'],format = 'mixed') #'%Y-%m-%d %H:%M:%S')
+
+#merge the generator and weather data
+df_solar = pd.merge(generation_data.drop(columns = ['PLANT_ID']), weather_data.drop(columns = ['PLANT_ID', 'SOURCE_KEY']), on='DATE_TIME')
+df_solar.sample(5).style.background_gradient(cmap='cool')
+
+# adding separate date, time, day, month ans weekday columns
+df_solar["DATE"] = pd.to_datetime(df_solar["DATE_TIME"]).dt.date
+df_solar["TIME"] = pd.to_datetime(df_solar["DATE_TIME"]).dt.time
+df_solar['DAY'] = pd.to_datetime(df_solar['DATE_TIME']).dt.day
+df_solar['MONTH'] = pd.to_datetime(df_solar['DATE_TIME']).dt.month
+df_solar['WEEK'] = pd.to_datetime(df_solar['DATE_TIME']).dt.weekday
+
+df_solar.info()
+
+# add hours and minutes for ml models
+df_solar['HOURS'] = pd.to_datetime(df_solar['TIME'],format='%H:%M:%S').dt.hour
+df_solar['MINUTES'] = pd.to_datetime(df_solar['TIME'],format='%H:%M:%S').dt.minute
+df_solar['TOTAL MINUTES PASS'] = df_solar['MINUTES'] + df_solar['HOURS']*60
+
+# add date as string column
+df_solar["DATE_STRING"] = df_solar["DATE"].astype(str) # add column with date as string
+df_solar["HOURS"] = df_solar["HOURS"].astype(str)
+df_solar["TIME"] = df_solar["TIME"].astype(str)
+
+df_solar.head(2)
+
+print(df_solar.isnull().sum())
+
+print(df_solar.describe().style.background_gradient(cmap='rainbow'))
+
+#convert from categorial to numerical
+from sklearn.preprocessing import LabelEncoder
+encoder = LabelEncoder()
+df_solar['SOURCE_KEY_NUMBER'] = encoder.fit_transform(df_solar['SOURCE_KEY'])
+
+df_solar.head()
+
+# visualise data
+sns.displot(data=df_solar, x="AMBIENT_TEMPERATURE", kde=True, bins = 100,color = "red", facecolor = "#3F7F7F",height = 5, aspect = 3.5);
+
+print(df_solar['DATE'].nunique())
+
+#ploting of DC power generation on per day basis
+solar_dc = df_solar.pivot_table(values='DC_POWER', index='TIME', columns='DATE')
+
+def Daywise_plot(data= None, row = None, col = None, title='DC Power'):
+    cols = data.columns # take all column
+    gp = plt.figure(figsize=(20,40)) 
+    
+    gp.subplots_adjust(wspace=0.2, hspace=0.5)
+    for i in range(1, len(cols)+1):
+        ax = gp.add_subplot(row,col, i)
+        data[cols[i-1]].plot(ax=ax, color='red')
+        ax.set_title('{} {}'.format(title, cols[i-1]),color='blue')
+        
+Daywise_plot(data=solar_dc, row=12, col=3)
+
+daily_dc = df_solar.groupby('DATE')['DC_POWER'].agg('sum')
+
+ax = daily_dc.sort_values(ascending=False).plot.bar(figsize=(17,5), legend=True,color='red')
+plt.title('Daily DC Power')
+#plt.show()
+
+#plotting of irradiation generation on per day basis using pivot table
+solar_irradiation = df_solar.pivot_table(values='IRRADIATION', index='TIME', columns='DATE')
+
+def Daywise_plot(data= None, row = None, col = None, title='IRRADIATION'):
+    cols = data.columns # take all column
+    gp = plt.figure(figsize=(20,40)) 
+    
+    gp.subplots_adjust(wspace=0.2, hspace=0.5)
+    for i in range(1, len(cols)+1):
+        ax = gp.add_subplot(row,col, i)
+        data[cols[i-1]].plot(ax=ax, color='blue')
+        ax.set_title('{} {}'.format(title, cols[i-1]),color='blue')
+        
+Daywise_plot(data=solar_irradiation, row=12, col=3)
+
+daily_irradiation = df_solar.groupby('DATE')['IRRADIATION'].agg('sum')
+
+daily_irradiation.sort_values(ascending=False).plot.bar(figsize=(17,5), legend=True,color='blue')
+plt.title('IRRADIATION')
+#plt.show()
+
+#Ambient temperature
+solar_ambiant_temp = df_solar.pivot_table(values='AMBIENT_TEMPERATURE', index='TIME', columns='DATE')
+
+def Daywise_plot(data= None, row = None, col = None, title='AMBIENT_TEMPERATURE'):
+    cols = data.columns # take all column
+    gp = plt.figure(figsize=(20,40)) 
+    
+    gp.subplots_adjust(wspace=0.2, hspace=0.5)
+    for i in range(1, len(cols)+1):
+        ax = gp.add_subplot(row,col, i)
+        data[cols[i-1]].plot(ax=ax, color='darkgreen')
+        ax.set_title('{} {}'.format(title, cols[i-1]),color='blue')
+        
+Daywise_plot(data=solar_ambiant_temp, row=12, col=3)
+
+daily_ambient_temp = df_solar.groupby('DATE')['AMBIENT_TEMPERATURE'].agg('sum')
+
+daily_ambient_temp.sort_values(ascending=False).plot.bar(figsize=(17,5), legend=True,color='darkgreen')
+plt.title('AMBIENT_TEMPERATURE')
+#plt.show()
+
+plt.figure(figsize=(16,16))
+
+date=["2020-05-15"]
+
+#prepare data 
+plt.subplot(411)
+sns.lineplot(data = df_solar, x = df_solar[df_solar["DATE_STRING"].isin(date)].DATE_TIME, y = df_solar[df_solar["DATE_STRING"].isin(date)].DC_POWER, label="DC_Power_Best",color='green')
+plt.title("DC Power Generation: {}" .format(date[0]))
+
+plt.subplot(412)
+sns.lineplot(data= df_solar, x = df_solar[df_solar["DATE_STRING"].isin(date)].DATE_TIME, y = df_solar[df_solar["DATE_STRING"].isin(date)].IRRADIATION, label="Irridation_Best",color='green');
+plt.title("Irradiation : {}" .format(date[0]))
+
+plt.subplot(413)
+sns.lineplot(data = df_solar, x = df_solar[df_solar["DATE_STRING"].isin(date)].DATE_TIME, y = df_solar[df_solar["DATE_STRING"].isin(date)].AMBIENT_TEMPERATURE, label="Ambient_Temperature_Best",color='green')
+sns.lineplot(data = df_solar, x = df_solar[df_solar["DATE_STRING"].isin(date)].DATE_TIME, y = df_solar[df_solar["DATE_STRING"].isin(date)].MODULE_TEMPERATURE, label="Module_Temperature_Best",color='blue')
+plt.title("Module Temperature & Ambient Temperature: {}" .format(date[0]))
+
+plt.tight_layout()
+#plt.show()
+
+solar_dc_power = df_solar[df_solar['DC_POWER'] > 0]['DC_POWER'].values
+solar_ac_power = df_solar[df_solar['AC_POWER'] > 0]['AC_POWER'].values
+
+solar_plant_eff = (np.max(solar_ac_power)/np.max(solar_dc_power ))*100
+print(f"Power ratio AC/DC (Efficiency) of Solar Power Plant:  {solar_plant_eff:0.3f} %")
+
+AC_list=[]
+for i in df_solar['AC_POWER']:
+    if i>0:
+        AC_list.append(i)
+AC_list
+#AC_list.sort()
+#AC_list.reverse()
+len(AC_list)
+
+#Here we take all nonzero DC values and plot them on histogram
+DC_list=[]
+for i in df_solar['DC_POWER']:
+    if i>0:
+        DC_list.append(i)
+DC_list
+DC_list.sort()
+DC_list.reverse()
+len(DC_list)
+
+plt.figure(figsize=(16,8))
+AC_list.sort()
+DC_list.sort()
+#print(DC_list)
+#DC_list.sort
+#res = [i / 10 for i in AC_list]
+eff = [i/j for i,j in zip(AC_list,DC_list)]
+
+plt.plot(AC_list,eff,color='green')
+plt.xlabel('Output power in kW')
+plt.ylabel('efficiency AC/DC')
+plt.title('Output power vs efficiency')
+
+#plt.show()
+
+df2 = df_solar.copy()
+
+#testing data with LSTM
+import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+import numpy as np
+import pandas as pd
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+
+# Fix random seed for reproducibility
+np.random.seed(7)
+
+# Create exogenous variables: Daily_yield, Ambient_Temperature, Module_tem[perature, Irradiation, DC_power
+features = df2[['DAILY_YIELD','TOTAL_YIELD','AMBIENT_TEMPERATURE','MODULE_TEMPERATURE','IRRADIATION','DC_POWER','AC_POWER']].values
+
+# Normalize the data
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scaler.fit_transform(features)
+
+# Function to create the dataset
+def create_dataset(data, time_step=1):
+    X, Y = [], []
+    for i in range(len(data) - time_step - 1):
+        X.append(data[i:(i + time_step), 1:])  # All features except 'DC_power'
+        Y.append(data[i + time_step, 0])  # Predicting 'DC_power'
+    return np.array(X), np.array(Y)
+
+# Set the time step (lookback window)
+time_step = 24  # Using the last 24 hours of data to predict the next hour
+
+# Create the dataset
+X, Y = create_dataset(scaled_data, time_step)
+
+# Reshape input to be [samples, time steps, features]
+X = X.reshape(X.shape[0], X.shape[1], X.shape[2])
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,Y,test_size=.2,random_state=21)
+
+# Build the LSTM model
+model = Sequential()
+model.add(LSTM(50, return_sequences=True, input_shape=(time_step, X_train.shape[2])))
+model.add(LSTM(50, return_sequences=False))
+model.add(Dense(25))
+model.add(Dense(1))
+
+# Compile the model
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+# Train the model
+#model.fit(X, Y, epochs=20, batch_size=32, verbose=2)
+model.fit(X_train, y_train, epochs=40, batch_size=32, verbose=2)
+
+# Predicting on the training data itself (for demonstration)
+train_predict = model.predict(X_test)
+
+# Inverse transform to get the actual values
+train_predict = scaler.inverse_transform(
+    np.hstack((train_predict, np.zeros((train_predict.shape[0], X_test.shape[2]))))
+)[:, 0]
+
+Y_actual = scaler.inverse_transform(
+    np.hstack((y_test.reshape(-1, 1), np.zeros((y_test.shape[0], X_test.shape[2]))))
+)[:, 0]
+
+
+# Plot the results
+plt.figure(figsize=(10, 6))
+plt.plot(Y_actual, label="Actual AC Power")
+plt.plot(train_predict, label="Predicted AC Power")
+plt.legend()
+plt.show()
+
+#calculate the r2_score
+from sklearn.metrics import r2_score
+R2_Score_dtr = round(r2_score(train_predict,Y_actual) * 100, 2)
+print("R2 Score for LSTM : ", R2_Score_dtr,"%")
+#Import library for saving model
+import pickle
+#Save the model to a file
+with open('results/solar-lstm-model.pkl', 'wb') as f:
+    pickle.dump(model, f)
+
